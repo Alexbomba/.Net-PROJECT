23051cf98d50219fb2d05a2a279e487c30f57612
S D <sdk267107@gmail.com>
2025-12-07 19:51:21 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace C____PROJECT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
